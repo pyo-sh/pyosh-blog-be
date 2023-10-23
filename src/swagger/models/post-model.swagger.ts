7ef2bbdad4f796a19017075dd2d82b94875f53c3
pyo-sh <pygosky@gmail.com>
2023-10-23 17:26:14 +0900

    feat: Swagger의 모델 추가 - Post

---
+++ src/swagger/models/post-model.swagger.ts
@@ -0,0 +1,53 @@
+import { ApiModel, ApiModelProperty } from "swagger-express-ts";
+import { PostEntity } from "@src/entities/post.entity";
+import { swaggerExamples } from "@src/swagger/examples";
+
+const { post } = swaggerExamples;
+
+@ApiModel({
+  name: "Post",
+  description: "블로그 글",
+})
+class SwaggerPostModel implements PostEntity {
+  @ApiModelProperty({
+    required: true,
+    example: post.id,
+  })
+  id: number;
+
+  @ApiModelProperty({
+    required: true,
+    example: post.content,
+  })
+  content: string;
+
+  @ApiModelProperty({
+    required: true,
+    example: post.createdAt,
+  })
+  createdAt: Date;
+
+  @ApiModelProperty({
+    required: true,
+    example: post.updatedAt,
+  })
+  updatedAt: Date;
+
+  @ApiModelProperty({
+    example: post.deletedAt,
+  })
+  deletedAt: Date | null;
+
+  @ApiModelProperty({
+    example: post.thumbnailId,
+  })
+  thumbnailId: number | null;
+
+  @ApiModelProperty({
+    required: true,
+    example: post.userId,
+  })
+  userId: number;
+}
+
+export default SwaggerPostModel;
