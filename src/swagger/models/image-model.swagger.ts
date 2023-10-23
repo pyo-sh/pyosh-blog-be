1b3c6e567ca48edb0f6dd3b6000c3eec0d158798
pyo-sh <pygosky@gmail.com>
2023-10-23 17:25:52 +0900

    feat: Swagger의 모델 추가 - Image

---
+++ src/swagger/models/image-model.swagger.ts
@@ -0,0 +1,36 @@
+import { ApiModel, ApiModelProperty } from "swagger-express-ts";
+import { ImageEntity } from "@src/entities/image.entity";
+import { swaggerExamples } from "@src/swagger/examples";
+
+const { image } = swaggerExamples;
+
+@ApiModel({
+  name: "Image",
+  description: "이미지 URL",
+})
+class SwaggerImageModel implements ImageEntity {
+  @ApiModelProperty({
+    required: true,
+    example: image.id,
+  })
+  id: number;
+
+  @ApiModelProperty({
+    required: true,
+    example: image.url,
+  })
+  url: string;
+
+  @ApiModelProperty({
+    required: true,
+    example: image.createdAt,
+  })
+  createdAt: Date;
+
+  @ApiModelProperty({
+    example: image.deletedAt,
+  })
+  deletedAt: Date | null;
+}
+
+export default SwaggerImageModel;
