68c700f7032e1e77541faf2a7b5971fc2cc261b3
pyo-sh <pygosky@gmail.com>
2023-10-23 17:42:11 +0900

    feat: Swagger 이름을 가진 파일들을 로드해서 swagger.json을 만들 수 있도록 설정

--- src/loaders/swagger.ts
+++ src/loaders/swagger.ts
@@ -1,4 +1,6 @@
 import type { Express } from "express";
+import path from "path";
+import { globSync } from "glob";
 import * as swagger from "swagger-express-ts";
 import swaggerUi from "swagger-ui-express";
 import envs from "@src/constants/env";
@@ -6,6 +8,15 @@ import { NodeEnv } from "@src/constants/node-env";
 
 export function loadSwagger(app: Express) {
   if (envs.NODE_ENV === NodeEnv.DEV) {
+    // *: need to load Swagger files (executing swagger-express-ts) (automatically)
+    const pattern = path.join(__dirname, "../swagger", "./**/*.swagger.ts");
+    const swaggerFiles = globSync(pattern);
+
+    swaggerFiles.forEach((fileName) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require(fileName)?.default;
+    });
+
     // CAUTION: need to be defined first then swagger-ui-express
     // for serving swagger.json
     app.use(
